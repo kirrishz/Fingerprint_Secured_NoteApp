95a80c80a96c81b21535b8d0c1caccdf6eb51d08
kirrishz <s.kirishanth97@gmail.com>
2022-05-26 23:24:19 +0530

    home,Addnote part Added

---
+++ app/src/main/java/com/example/noteme/AddNote.java
@@ -0,0 +1,20 @@
+package com.example.noteme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class AddNote extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.listOfNotes);
+    }
+}
---
+++ app/src/main/java/com/example/noteme/SimpleDatabase.java
@@ -0,0 +1,4 @@
+package com.example.noteme;
+
+public class SimpleDatabase {
+}
